11f1c3c78f1447ddcf131ae70b17ccef26cbf1c6
Abha Saxena <abha.saxena@sapiens.com>
2023-08-06 10:07:33 +0530

    feat: log workout app

---
+++ theory.js
@@ -0,0 +1,33 @@
+//Planning Steps
+/**
+ 1. USER Stories - a user story is basically a description of the application's functionality from the user's perspective. And then all the user stories put together will clearly describe the functionality of the entire application. So user stories are basically a high level overview of the whole application, which will allow us developers to determine the exact features that we need to implement in order to make the user stories actually work as intended.
+
+ 2. Features 
+ 3. Flowchart
+ 4. Architrecture -architecture simply means how we will organize our code, and what JavaScript features we will use. So the project's architecture is essentially what holds all the code together. It gives us a structure in which we can then develop the application's functionality.  
+ */
+
+// USER STORY
+/**
+ * I want to log my running workouts with location, distance, time, pace, and steps per minute, so that I can keep a log of all my running.
+ * As a user, I want to log my cycling workouts with location, distance, time, speed, and elevation gain, so I can keep a log of all my cycling.
+ * As a user, I want to see all my workouts at a glance so I can easily track my progress over time.
+ * As a user, I also want to see all my workouts on a map so I can easily check where I work out the most.
+ *  As a user, I want to see all my workouts when I leave the app, and come back later, so that I can keep using the app over time.  */
+
+// Features -> based on the user stories
+/**
+ First User story
+ *  Need Map
+ * Geolocation to display map at current location
+ * form input distance time pace, steps/miunure
+ Second
+ * form input distance time pace, elevation gain
+ Third
+ * Display all workouts in a list
+ Fourth
+ * Display ALl workouts in the map
+ Fifth
+ * Store workout data in the browser using local storage API
+ * On pageload, read the saved data from local stoargae and display
+ */
